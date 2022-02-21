29201c9abfa065c909a91be60cac2a27c3fd0791
zhongmingxing <mingxing.zhong@irootcloud.com>
2022-02-21 16:41:40 +0800

    feat: add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { Button, ConfigProvider, DatePicker, Space } from "antd";
+import { ConfigProvider } from "antd";
 import zhCN from "antd/lib/locale/zh_CN"; // 由于 antd 组件的默认文案是英文，所以需要修改为中文
 import moment from "moment";
 import "moment/locale/zh-cn";
 import React from "react";
+import AppLayout from "./layout";
 import "./styles/index.less";
 
 moment.locale("zh-cn");
@@ -10,10 +11,7 @@ moment.locale("zh-cn");
 const App = () => {
   return (
     <ConfigProvider locale={zhCN}>
-      <Space>
-        <Button>点我！</Button>
-        <DatePicker />
-      </Space>
+      <AppLayout />
     </ConfigProvider>
   );
 };
---
+++ src/layout/index.tsx
@@ -0,0 +1,66 @@
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Breadcrumb, Layout, Menu } from "antd";
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+const AppLayout: React.FC = () => {
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider collapsible>
+        <div
+          style={{
+            height: 32,
+            margin: 16,
+            background: "rgba(255, 255, 255, 0.2)",
+          }}
+        />
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Option 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            Option 2
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+            <Menu.Item key="3">Tom</Menu.Item>
+            <Menu.Item key="4">Bill</Menu.Item>
+            <Menu.Item key="5">Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="9" icon={<FileOutlined />}>
+            Files
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header style={{ padding: 0 }} />
+        <Content style={{ margin: "0 16px" }}>
+          <Layout style={{ minHeight: "100%" }}>
+            <Breadcrumb style={{ margin: "16px 0" }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content
+              style={{ margin: "0 16px", background: "#ffffff" }}
+            ></Content>
+          </Layout>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default AppLayout;
